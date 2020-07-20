038b19304391de9d147f19548fd7e07c2c1c8f94
Thomas Eberwein <thomas@thomastraum.com>
2020-07-21 00:17:27 +0100

    used imports

--- package.js
+++ package.js
@@ -1,19 +1,28 @@
 Package.describe({
-  name: "tmeasday:presence",
+  name: "thomastraum:presence",
   summary: "A package to help track users' presence",
   version: "1.0.6",
   git: "https://github.com/dburles/meteor-presence.git"
 });
 
 Package.onUse(function (api) {
-  api.versionsFrom('1.0');
-  api.use('tracker', 'client');
-  api.use('mongo');
-  api.use('underscore');
+  api.versionsFrom('1.3');
+  api.use('modules');
+  api.use([
+    'ecmascript',
+    'mongo',
+    'underscore',
+    'tracker',
+    // 'client',
+  ]);
+  // api.versionsFrom('1.3');
+  // api.use('tracker', 'client', 'ecmascript');
+  // api.use('mongo');
+  // api.use('underscore');
 
-  api.addFiles('lib/common.js');
-  api.addFiles('lib/client.js', 'client');
-  api.addFiles('lib/server.js', 'server');
+  api.addFiles('./lib/common.js');
+  api.addFiles('./lib/client.js', 'client');
+  api.addFiles('./lib/server.js', 'server');
 
   api.export('Presences');
   api.export('Presence');
